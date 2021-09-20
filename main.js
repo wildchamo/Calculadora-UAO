01ba4d7847c0acc8b036ac28cca732270adf66c7
Jose Luis <wildchamo@gmail.com>
2021-09-20 18:49:12 -0500

    extraer valores de los input

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-
-
-    let input1=document.getElementById("valor1");
-    let valor1=input1.value;
-    let input2=document.getElementById("valor2");
-    let valor2=input2.value;
+ let input1=document.getElementById("valor1");
+ let valor1=input1.value;
+let input2=document.getElementById("valor2");
+let valor2=input2.value;
+let operacion= document.getElementById("inputOperacion");
+let valorOperacion= operacion.value;
 
     function imprimir(){
-        console.log(valor1);
+        console.log(valorOperacion);
     }
