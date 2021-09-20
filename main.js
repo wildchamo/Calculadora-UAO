1fce09245e8e2c9924531d449ad5ddd1915a7358
Jose Luis <wildchamo@gmail.com>
2021-09-20 18:41:33 -0500

    agrego el archivo de js para la logica

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+    let input1=document.getElementById("valor1");
+    let valor1=input1.value;
+    let input2=document.getElementById("valor2");
+    let valor2=input2.value;
+
+    function imprimir(){
+        console.log(valor1);
+    }
