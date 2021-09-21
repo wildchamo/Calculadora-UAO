1238de37027fc8618375728308b8b54dcd768fde
Jose Luis <wildchamo@gmail.com>
2021-09-20 19:03:07 -0500

    creación de las funciones que permitirán realizar operaciones

--- main.js
+++ main.js
@@ -5,6 +5,42 @@ let valor2=input2.value;
 let operacion= document.getElementById("inputOperacion");
 let valorOperacion= operacion.value;
 
-    function imprimir(){
-        console.log(valorOperacion);
+
+//let valorvalor =sumar(valor1,valor2)
+//
+
+
+
+function sumar(a,b){
+    return a+b;
+}
+function restar(a,b){
+    return a-b
+}
+function multiplicar(a,b){
+    return a*b;
+}
+
+function dividir (a,b){
+    return a/b;
+}
+
+
+function Calcular(valorOperacion){
+    let res = 0
+
+    if(valorOperacion==="Sumar"){
+        sumar(valor1, valor2);
+    }else if(valorOperacion==="Restar"){
+        restar(valor1, valor2);
+    }else if(valorOperacion==="Multiplicar"){
+        multiplicar(valor1,valor2);
+    }else{
+        dividir(valor1,valor2)
     }
+    resultado.innerHTML = "<strong>"+ res + "</strong>";
+}
+
+function OnClick(){
+    Calcular(valorOperacion);
+}
